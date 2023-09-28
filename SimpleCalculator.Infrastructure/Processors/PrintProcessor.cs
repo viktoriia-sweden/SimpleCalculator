7a81bbbd61404b1a2e0449d7bc9db678db9dd9fa
Victoria Voroshilova <Victoria.Voroshilova@kaspersky.com>
2023-09-28 22:29:33 +0400

    added non circular print method

--- SimpleCalculator.Infrastructure/Processors/PrintProcessor.cs
+++ SimpleCalculator.Infrastructure/Processors/PrintProcessor.cs
@@ -59,6 +59,37 @@ namespace SimpleCalculator.Infrastructure.Processors
 			_ => throw new ArgumentOutOfRangeException(nameof(operation), $"Invalid operation: {operation}"),
 		};
 
+		/// <summary>
+		/// The method is not used. I left it here just to show an approach of handling circular dependency when they are not allowed.
+		/// </summary>
+		private long CalculateWithoutCircularDependencies(string register)
+		{
+			if (checkedRegisters.Contains(register))
+			{
+				throw new InvalidOperationException($"Program cannot evaluate register '{register}' due to circular dependency.");
+			}
+
+			while (_registerRepository.GetCommandsCount(register) > 0)
+			{
+				var command = _registerRepository.GetCommand(register);
+				var registerValue = _registerRepository.Get(register);
+
+				if (long.TryParse(command.Operand, out var operandValue))
+				{
+					_registerRepository.Save(register, ApplyOperation(registerValue, command.Operation, operandValue));
+				}
+				else
+				{
+					checkedRegisters.Add(register);
+					_registerRepository.Save(register, ApplyOperation(registerValue, command.Operation, CalculateWithoutCircularDependencies(command.Operand)));
+				}
+			}
+
+			checkedRegisters.Remove(register);
+			return _registerRepository.Get(register);
+		}
+
+		private readonly HashSet<string> checkedRegisters = new();
 		private readonly IRegisterRepository _registerRepository;
 		private readonly IConsoleService _consoleService;
 	}
